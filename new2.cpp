90a492f19cdead030209452fbd42a5cc95fa207d
Bassam-Adnan <33580007+Bassam-Adnan@users.noreply.github.com>
2017-11-11 21:59:14 +0300

    Simple program for cin & cout of array

---
+++ new2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+int array[5];
+for(int i=0;i<5;i++)
+cin>>array[i];
+cout<<endl;
+for(int i=0;i<5;i++)
+cout<<array[i]<<"\t";	
+	
+getch();
+return 0;
+}	
